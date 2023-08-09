2d3bb9effd11530717866450ab9383ef9abf775b
Veda452 <vedav6343@gmail.com>
2023-08-10 02:33:52 +0530

    change

--- server.js
+++ server.js
@@ -5,9 +5,6 @@ import DefaultData from './default.js';
 import Router from './routes/route.js';
 import cors from 'cors';
 import bodyParser from 'body-parser';
-import passport from 'passport';
-import cookieSession from 'cookie-session';
-import GoogleStrategy from 'passport-google-oauth20';
 
 
 
@@ -15,47 +12,7 @@ dotenv.config();
 
 const app=express();
 
-
-
-passport.use(
-	new GoogleStrategy(
-		{
-			clientID: process.env.CLIENT_ID,
-			clientSecret: process.env.CLIENT_SECRET,
-			callbackURL: "/auth/google/callback",
-			scope: ["profile", "email"],
-		},
-	    function (accessToken, refreshToken, profile, callback) {
-            console.log(accessToken);
-            console.log(refreshToken);
-			callback(null, profile);
-		}
-	)
-);
-
-passport.serializeUser((user, done) => {
-	done(null, user);
-});
-
-passport.deserializeUser((user, done) => {
-	done(null, user);
-});
-
-app.use(
-	cookieSession({
-		name: "session",
-		keys: ["cyberwolve"],
-		maxAge: 24 * 60 * 60 * 100,
-	})
-);
-
-app.use(passport.initialize());
-app.use(passport.session());
-app.use(cors({
-    origin:'http://localhost:3000',
-    methods:'GET,POST,PUT,DELETE',
-    credentials:true,
-}));
+app.use(cors());
 app.use(bodyParser.json({extended:true}));
 app.use(bodyParser.urlencoded({extended:true}));
 app.use('/', Router);
